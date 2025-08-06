f0566b3d8cf823dc0cdafdef5fc5f9b87f9e0f2e
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-08-06 10:54:21 -0300

    Array.Matriz

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// //matriz
+
+// let matriz = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+// console.log(`Valor da 1 linha e 2 coluna é: ${matriz[0][1]}`);
+
+let alunos = [
+    ['João', 10],
+    ['José', 6],
+    ['Maria', 7.5],
+]
+ for(let i = 0; i < alunos.length; i++){
+    console.log(`${alunos[i][0]} tirou ${alunos[i][1]}`);
+} 
