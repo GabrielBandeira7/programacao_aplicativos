05a93fd5a8a8696ab0966ce536a9ed2347ea149d
GabrielBandeira7 <bandeiragabriel07@gmail.com>
2025-08-06 10:51:14 -0300

    Update atividade3.d.js

--- atividade3.d.js
+++ atividade3.d.js
@@ -1,11 +1,24 @@
-const nome = ["Caio", "Matheus"];
-const idade = [12, 13];
+const prompt = requese('prompt-sync')();
 
-nome.forEach(function(element){
-    console.log("Nome: ", element);
-});
+let totalPessoas = parseInt(prompt("Quantas pessoas deseja cadastrar?"));
 
-idade.forEach(function(element){
-    console.log("idade: ", element);
-});
+let pessoaMaisVelha = { nome: "", idade: -Infinity };
+let pessoaMaisJovem = { nome: "", idade: Infinity };
 
+for (let i = 0; i < totalPessoas; i++) {
+    let nome = prompt(`Digite o nome da pessoa ${i + 1}:`);
+    let idade = parseInt(prompt(`Digite a idade de ${nome}:`));
+
+    if (idade > pessoaMaisVelha.idade) {
+        pessoaMaisVelha.nome = nome;
+        pessoaMaisVelha.idade = idade;
+    }
+
+    if (idade < pessoaMaisJovem.idade) {
+        pessoaMaisJovem.nome = nome;
+        pessoaMaisJovem.idade = idade;
+    }
+}
+
+console.log(`Pessoa mais velha: ${pessoaMaisVelha.nome}, ${pessoaMaisVelha.idade} anos`);
+console.log(`Pessoa mais jovem: ${pessoaMaisJovem.nome}, ${pessoaMaisJovem.idade} anos`);
